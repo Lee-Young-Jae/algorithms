bb11759617f0164768cc0da7b9e05fb73ca29d7b
Lee-Young-Jae <ori_@kakao.com>
2024-02-02 20:05:37 +0900

    feat: Test Pass

--- programmers-JS/Lv2/주식가격/index.js
+++ programmers-JS/Lv2/주식가격/index.js
@@ -1,15 +1,26 @@
 function solution(prices) {
-  const queue = [...prices].slice(1, prices.length);
+  const result = new Array(prices.length)
+    .fill(0)
+    .map((_, index) => prices.length - index - 1);
+  const stack = [];
 
-  return prices.map((price, sec) => {
-    const declineIdx = queue.findIndex((stock) => price > stock);
-    queue.shift();
-    if (declineIdx >= 0) {
-      return declineIdx + 1;
+  prices.forEach((price, index) => {
+    if (stack.length) {
+      let sec = 1;
+      let prevStock = stack[stack.length - 1][0];
+      while (stack.length && prevStock > price) {
+        const [_, prevIndex] = stack.pop();
+        prevStock = stack.length ? stack[stack.length - 1][0] : 0;
+        result[prevIndex] = index - prevIndex;
+        sec++;
+      }
     }
-    return prices.length - (sec + 1);
+    stack.push([price, index]);
   });
-}
 
+  return result;
+}
 console.log(solution([1, 2, 3, 2, 3])); // [4, 3, 1, 1, 0]
 console.log(solution([1, 2, 3, 2, 3, 1])); // [5, 4, 1, 2, 1, 0]
+console.log(solution([1, 2, 3, 2, 3, 1, 3])); // [6, 4, 1, 2, 1, 1, 0]
+console.log(solution([3, 2, 4, 1, 1])); // [1, 2, 1, 1, 0]
